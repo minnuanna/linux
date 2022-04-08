5216c1aadc3d6e0879ad52b953ebb92d0ba910bb
minnuanna <anjalysuresh199@gmail.com>
2022-04-08 16:36:07 +0500

    reverse of a number

---
+++ reverse.sh
@@ -0,0 +1,16 @@
+echo "enter the digit"
+read n
+b=$n
+s=0
+while [ $n -gt 0 ]
+do
+r=`expr $n % 10`
+s=`expr $s \* 10 + $r`
+if [ $s -eq 0 -a $r -eq 0 ]
+then
+echo $s
+fi
+n=`expr $n / 10`
+done
+echo "reverse is $s"
+
